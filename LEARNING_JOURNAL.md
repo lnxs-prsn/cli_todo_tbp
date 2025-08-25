1fab84e69f112327c971ff0be097ae437836b3e0
mr <lnxsprsn@meiliboxi.fi>
2025-08-25 19:34:53 +0300

    updated the learning journal

--- LEARNING_JOURNAL.md
+++ LEARNING_JOURNAL.md
@@ -2,4 +2,8 @@
     - read the task blue print and the task check list to find out where I left last time 
     as I was sick for 4 days and unable to continue the project-
     - I was in middle of researching the json files and how to use them to persist the to do tasks
-    
+    - added learning journal to make catch up from now on easier
+    - added comments to the code so catching up and possible debugging becomes easier
+    - began testing how to access data from json
+      - I stored data as string or dictionary am guessing as string because when I try to print the dictionary as for loop 
+      - I get only the first key as I stored it as {key: {values}}
--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ import argparse
 import datetime
 import json
 
-
+# this function creates an task and stores it to json file
 def add_task():
     parser = argparse.ArgumentParser(description='cli todo')
 
@@ -11,18 +11,19 @@ def add_task():
     parser.add_argument('end_date', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d').date(), help='write end date of the task',)
     
     args = parser.parse_args()
-    
+    # lines below create file and store to the file the tasks 
     print(args.task_name, args.task_description, args.end_date)
     with open('./cli_todo.json', 'w', encoding='utf-8') as ff:
-        json.dump({'task_name':f'{args.task_name}', 'task_description':f'{args.task_description}', 'end_date':f'{args.end_date}'}, ff)
+        json.dump({f'{args.task_name}':{'task_name':f'{args.task_name}', 'task_description':f'{args.task_description}', 'end_date':f'{args.end_date}'}}, ff)
 
 # add_task()
 
-
+# this function opens the json file and print data on the file to terminal
 def edit_task():
     with open('./cli_todo.json', 'r') as file:
         data= json.load(file)
-    print(data)
+    for x,y in data:
+        print(x)
 
 
 edit_task()
